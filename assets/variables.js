a30d6997ffd1bc182e944c2c52aa340a9f9d1cba
hdyn29 <74176019+hdyn29@users.noreply.github.com>
2020-11-09 15:14:27 +0700

    Update variables.js

--- assets/variables.js
+++ assets/variables.js
@@ -7,13 +7,13 @@ const variables = {
     night: "Good Night",
   },
   quotes: [
-    { quote: "I'm sorry for loving you.", author: "standinhsd" },
-    { quote: "I love you.", author: "Brian" },
-    { quote: "Aku sayang kamu!", author: "Ian" },
-    { quote: "Ian sayang Ava ♥", author: "Ian-nya Ava!" },
-    { quote: "Love chuuu", author: "Bribri" },
+    { quote: "I'm sorry for loving you.", author: "Hadiyan" },
+    { quote: "I love you.", author: "Hadiyan" },
+    { quote: "Aku sayang kamu!", author: "Hadiyan" },
+    { quote: "Aku sayang Piola ♥", author: "hadiyan" },
+    { quote: "Love chuuu", author: "hadiyan" },
   ],
-  nicknames: ["Ava", "Avakuu", "Sayang", "Babe", "Cintaku", "Cinta"],
+  nicknames: ["Pio", "Piola", "Sayang", "bee", "Cintaku", "Cinta"],
   generated: ["{greetings}, {nicknames}!"],
 };
 
